fea7482f6e4ce94f05275c7ecc887168ba7b09c3
Maksim Gurtovenko <maksim@gurtovenko.name>
2017-12-28 17:50:00 +0700

    Stitch multiple 1-deg tiles to smmoth coverage

--- build_relieff_cli.py
+++ build_relieff_cli.py
@@ -17,6 +17,15 @@ parser.add_argument("-O", "--output", help="Output directory",
                     default="contours-json")
 parser.add_argument("-i", "--interval",
                     default=10, help="Contours interval in meters, default 10")
+parser.add_argument("-t", "--mapnik-config-template",
+                    default='contours.xml', help="Template of Mapnik config")
+parser.add_argument("-C", "--mapnik-config",
+                    default='mapnik-config',
+                    help="Mapnik config files directory")
+parser.add_argument("-d", "--dry-run",
+                    default='false',
+                    help="Dont generate contours")
+
 args = parser.parse_args()
 
 files = os.listdir(args.input)
@@ -25,10 +34,14 @@ files = sorted(files)
 
 for f in tqdm(files):
     fin = os.path.join(args.input, f)
+    name = re.sub(r'(.*?).hgt.tif$', "\\1", f)
     fout = re.sub(r'(.*?).hgt.tif$', "\\1.json", f)
     fout = os.path.join(args.output, fout)
     print("Readind %s to %s" % (fin, fout))
-    find_contours(fin, fout, int(args.interval))
-#
-#     features = read_contours(fin)
-#chmod
+    if not args.dry_run:
+        find_contours(fin, fout, int(args.interval))
+    with open(args.mapnik_config_template) as rh:
+        tmpl = str(rh.read())
+        tmpl = re.sub(r'\{NAME\}', name, tmpl)
+        with open(os.path.join(args.mapnik_config, name + ".xml"), 'w') as wh:
+            wh.write(tmpl)
